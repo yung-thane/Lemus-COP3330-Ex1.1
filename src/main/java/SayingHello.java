5b492977b8ca5c351eb5f535b052bdb50bde1fe1
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-10 21:56:26 -0400

    Commit 3 Final

--- src/main/java/SayingHello.java
+++ src/main/java/SayingHello.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class SayingHello {
    public static void main(String[] args){
-       System.out.print("What is your name?");
+       System.out.print("What is your name? ");
        Scanner name = new Scanner(System.in);
 
         String name1 = name.nextLine();
